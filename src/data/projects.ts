8346e1340c478c860b6192bc313f1bdf5c271a6f
AarhamH <aarham.haider@gmail.com>
2023-07-27 00:52:08 -0700

    Created project grids

--- src/data/projects.ts
+++ src/data/projects.ts
@@ -1,3 +1,6 @@
+import projImg1 from '../content/images/music-player.png'
+import projImg2 from '../content/images/nba-quicksearch.png'
+
 export type Project = {
   title: string;
   techs: string[];
@@ -11,19 +14,25 @@ const projects: Project[] = [
     title: "MVVM Music Player",
     techs: ["C#", "XAML", "Entity Framework Core","SQLite"],
     link: "https://github.com/MaeWolff/dictionary-app",
-    image: ""
+    image: projImg1
   },
   {
     title: "NBA Quicksearch",
     techs: ["Javascript (React Native)", "Expo", "API"],
     link: "https://www.linablidi.fr/",
-    image:""
+    image: projImg2
   },
   {
     title: "Image Editor",
     techs: ["Python (PyGame, Numpy)"],
     link: "/",
-    image: ""
+    image: projImg2
+  },
+  {
+    title: "Image Editor",
+    techs: ["Python (PyGame, Numpy)"],
+    link: "/",
+    image: projImg2
   },
 ];
 
