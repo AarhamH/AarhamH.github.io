d6960e17abcaea2b1480f015b7d2f7c6a5f6718a
AarhamH <aarham.haider@gmail.com>
2023-07-26 23:21:13 -0700

    Updated project card style

--- src/data/projects.ts
+++ src/data/projects.ts
@@ -2,6 +2,7 @@ export type Project = {
   title: string;
   techs: string[];
   link: string;
+  image: string;
   isComingSoon?: boolean;
 };
 
@@ -10,16 +11,19 @@ const projects: Project[] = [
     title: "MVVM Music Player",
     techs: ["C#", "XAML", "Entity Framework Core","SQLite"],
     link: "https://github.com/MaeWolff/dictionary-app",
+    image: ""
   },
   {
     title: "NBA Quicksearch",
     techs: ["Javascript (React Native)", "Expo", "API"],
     link: "https://www.linablidi.fr/",
+    image:""
   },
   {
     title: "Image Editor",
     techs: ["Python (PyGame, Numpy)"],
     link: "/",
+    image: ""
   },
 ];
 
