b47c32f7c3b84ea454dfe8fe3f22c6141fb258aa
AarhamH <aarham.haider@gmail.com>
2023-07-31 19:11:44 -0700

    Updated Socials

--- src/data/presentation.ts
+++ src/data/presentation.ts
@@ -1,6 +1,11 @@
+import socialImg1 from "../content/socials/_github.svg"
+import socialImg2 from "../content/socials/_linkedin.svg"
+
+
 type Social = {
   label: string;
   link: string;
+  image: string;
 };
 
 type Presentation = {
@@ -15,19 +20,18 @@ const presentation: Presentation = {
   mail: "aarham.haider@gmail.com",
   title: "Hey, I’m Aarham!",
   description:"I am a *third year CS Student @ SFU*, expecting to graduate in May 2024. I have a passion for *problem solving* and interest in building services that cater to the needs of many people. As a developer, I have honed skills in various technologies.\n Currently, I am particularly enjoying *C#* development within *.NET*, and have built programs using *ASP.NET Core, WPF, and Blazor*, as well as video games in *Unity*. I am also experienced in other technolgies, such as *Python, C/C++*, and *Javascript React/React Native*. Apart from building software, I enjoy classical music, basketball, and model designing in Blender!",
-  education: "Simon Fraser University — Candidate for Bachelor's Of Applied Science in Computing Science (expected 2024)",
+  education: "Simon Fraser University — Bachelor's Of Applied Science in Computing Science (2021-present)",
   socials: [
     {
-      label: "Twiiter",
-      link: "https://twitter.com/itsstormzz_",
-    },
-    {
-      label: "Bento",
-      link: "https://bento.me/m-wolff",
-    },
-    {
       label: "Github",
-      link: "https://github.com/MaeWolff",
+      link: "https://github.com/AarhamH",
+      image: socialImg1
+    },
+    {
+      label: "LinkedIn",
+      link: "https://ca.linkedin.com/in/aarham-haider-5a0a4a1bb",
+      image: socialImg2
+
     },
   ],
 };
