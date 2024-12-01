5ec596a71a15326608b54830186b5ccbfd665a4a
AarhamH <ahaider@cayenta.com>
2024-12-01 04:39:51 -0800

    Added better images

--- src/data/presentation.ts
+++ src/data/presentation.ts
@@ -14,15 +14,23 @@ type Presentation = {
   mail: string;
   title: string;
   description: string;
-  education: string;
+  education: {
+    institution: string,
+    degree: string
+    gpa: string
+  };
   socials: Social[];
 };
 
 const presentation: Presentation = {
   mail: "aarham.haider@gmail.com",
   title: "hey, i’m aarham!",
-  description:"I am interested in solving problems and building fun software. Nearing 2 years of professional software engineering experience, I have worked in diverse software teams and expanded my domain knowledge in an array of technologies, software patterns, and techniques. As a hobby, I like messing around with Haskell and LISP dialects. Apart from software, I enjoy classical music, basketball, and poking trees.",
-  education: "Simon Fraser University | Bachelor's of Applied Science in Computer Science | 3.75/4.33 GPA",
+  description:"I am interested in solving problems and building fun software. Nearing 2 years software engineering experience, I have worked in diverse software teams and expanded my domain knowledge in an array of technologies, software patterns, and techniques. I mainly work in Go, but also loved hacking around with Rust and Haskell. Apart from software, I enjoy classical music, preacher curls, and poking trees.",
+  education: {
+    institution: "Simon Fraser University",
+    degree: "Bachelor's of Applied Science in Computing Science",
+    gpa: "3.84"
+  },
   socials: [
     {
       label: "Github",
